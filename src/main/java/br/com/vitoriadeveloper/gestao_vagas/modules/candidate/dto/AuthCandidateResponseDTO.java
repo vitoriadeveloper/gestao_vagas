21a60a0170f72ea6fa674211deb7d534fbd7e289
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-08-10 14:39:10 -0300

    [FEAT] - auth of profile created

--- src/main/java/br/com/vitoriadeveloper/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
+++ src/main/java/br/com/vitoriadeveloper/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -5,10 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthCandidateResponseDTO {
     private String access_token;
+    private Long expires_in;
 }
