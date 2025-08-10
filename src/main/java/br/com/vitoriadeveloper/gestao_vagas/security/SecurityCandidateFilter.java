deb52d60616f3c98611c58057bc11f9c586ae95e
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-08-10 19:09:25 -0300

    [FEAT] - jwt security candidate filter and jwt candidate provider implemented

---
+++ src/main/java/br/com/vitoriadeveloper/gestao_vagas/security/SecurityCandidateFilter.java
@@ -0,0 +1,52 @@
+package br.com.vitoriadeveloper.gestao_vagas.security;
+
+import br.com.vitoriadeveloper.gestao_vagas.providers.JWTCandidateProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityCandidateFilter extends OncePerRequestFilter {
+    @Autowired
+    private JWTCandidateProvider jwtCandidateProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String uri = request.getRequestURI();
+        String method = request.getMethod();
+        // Pular verificação nessas rotas públicas
+        if (("/candidate".equals(uri) && "POST".equalsIgnoreCase(method)) ||
+                "/candidate/auth".equals(uri)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(null);
+        String header = request.getHeader("Authorization");
+
+        if (uri.startsWith("/candidate")) {
+            if (header != null) {
+                var token = this.jwtCandidateProvider.validateToken(header);
+                if (token == null) {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    return;
+                }
+                request.setAttribute("candidate_id", token.getSubject());
+                System.out.println("======= TOKEN =======");
+                System.out.println(token.getClaim("roles"));
+            } else {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
