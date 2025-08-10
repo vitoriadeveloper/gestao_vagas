8e9b84564774e05bcb35183b5bd7971d97eb4568
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-08-10 19:10:24 -0300

    [FEAT] - implementing conditions at token validation

--- src/main/java/br/com/vitoriadeveloper/gestao_vagas/security/SecurityFilter.java
+++ src/main/java/br/com/vitoriadeveloper/gestao_vagas/security/SecurityFilter.java
@@ -19,23 +19,28 @@ import jakarta.servlet.http.HttpServletResponse;
 public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private JWTProvider jwtProvider;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
-        if(header != null){
-            var subjectToken = this.jwtProvider.validateToken(header);
 
-            if(subjectToken.isEmpty()){
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
+        if (request.getRequestURI().startsWith("/company")) {
+            if (header != null) {
+                var subjectToken = this.jwtProvider.validateToken(header);
+
+                if (subjectToken.isEmpty()) {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    return;
+                }
+                request.setAttribute("company_id", subjectToken);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null, Collections.emptyList());
+                SecurityContextHolder.getContext().setAuthentication(auth);
             }
-            request.setAttribute("company_id", subjectToken);
-            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null, Collections.emptyList());
-            SecurityContextHolder.getContext().setAuthentication(auth);
+            filterChain.doFilter(request, response);
         }
-       filterChain.doFilter(request, response);
+
     }
-    
+
 }
